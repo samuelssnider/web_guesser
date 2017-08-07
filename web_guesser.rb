8bf260e76614415b39ab25d8926c52b021158148
samuelssnider <sam_redins@yahoo.com>
2017-08-06 20:39:27 -0600

    IO: eleventh

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+
+
+random = rand(100) 
+number = random
+
+
 get '/' do
-	"the secret number is #{rand(100)}"
+	"the secret number #{number}"
 end
+

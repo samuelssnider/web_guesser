952b5a88267bc3d947df1f3c9400974e850a44e6
samuelssnider <sam_redins@yahoo.com>
2017-08-06 22:15:18 -0600

    circles of death

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,19 +5,13 @@ require 'sinatra/reloader'
 
 random  = rand(100) 
 number  = random
-# message = ""
-
+message = ""
 
 
 
 get '/' do
 	erb :index, :locals   => {:message   => message}
 	erb :index, :locals   => {:number    => number }
-	guess = params['guess'].to_i 
-	message = ""
-	if guess > number
-	 message = "too high"
-	end
 	
 end
 
